9a6fc17fa812330bf32de6a2c4d807cf1fae1be5
romanokeser <romanokeser@gmail.com>
2024-04-12 10:15:02 +0200

    Drawing in canvas

---
+++ HeatMap.cs
@@ -0,0 +1,136 @@
+﻿using MouseRecording.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MouseRecording
+{
+	public class HeatMap : FrameworkElement
+	{
+		private const int GradientNumber = 256; // Number of color gradients
+
+		private VisualCollection heatMapVisuals;
+		private List<HeatPoint> heatPoints = new List<HeatPoint>(); // Points of the heat map
+		private readonly RadialGradientBrush[] brushes; // Monochrome brushes used for heat map drawing
+
+		
+		public HeatMap()
+		{
+
+			heatMapVisuals = new VisualCollection(this);
+			brushes = new RadialGradientBrush[GradientNumber];
+
+			for (int i = 0; i < brushes.Length; i++)
+			{
+				brushes[i] = new RadialGradientBrush(Color.FromArgb((byte)i, 255, 255, 255), Color.FromArgb(0, 255, 255, 255));
+			}
+		}
+
+		protected override int VisualChildrenCount => heatMapVisuals.Count;
+
+		protected override Visual GetVisualChild(int index)
+		{
+			if (index < 0 || index >= heatMapVisuals.Count)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			return heatMapVisuals[index];
+		}
+
+		public void AddHeatPoint(int x, int y, byte intensity)
+		{
+			heatPoints.Add(new HeatPoint(x, y, intensity));
+		}
+
+		public void Clear()
+		{
+			heatPoints.Clear();
+		}
+
+		public void Render()
+		{
+			double widthHeight;
+			var mouseCoordinates = DatabaseHelper.GetMouseCoordinates();
+
+			heatMapVisuals.Clear();
+
+			DrawingVisual drawingVisual = new DrawingVisual();
+
+			using (DrawingContext dc = drawingVisual.RenderOpen())
+			{
+				foreach (DataRow row in mouseCoordinates.Rows)
+				{
+					int x = Convert.ToInt32(row["x_coord"]);
+					int y = Convert.ToInt32(row["y_coord"]);
+					byte intensity = 255; // Assuming the intensity value for each point
+
+					widthHeight = intensity / 5;
+
+					dc.DrawRectangle(brushes[intensity], null, new Rect(x - widthHeight / 2, y - widthHeight / 2, widthHeight, widthHeight));
+				}
+
+				heatMapVisuals.Add(drawingVisual);
+			}
+		}
+
+		public void RenderAndExport(string filePath)
+		{
+			var mouseCoordinates = DatabaseHelper.GetMouseCoordinates();
+
+			// Create a RenderTargetBitmap to render the heatmap
+			RenderTargetBitmap rtb = new RenderTargetBitmap((int)ActualWidth, (int)ActualHeight, 96, 96, PixelFormats.Pbgra32);
+
+			DrawingVisual drawingVisual = new DrawingVisual();
+
+			using (DrawingContext dc = drawingVisual.RenderOpen())
+			{
+				foreach (DataRow row in mouseCoordinates.Rows)
+				{
+					int x = Convert.ToInt32(row["x_coord"]);
+					int y = Convert.ToInt32(row["y_coord"]);
+					byte intensity = 255; // Assuming the intensity value for each point
+
+					double widthHeight = intensity / 5;
+
+					// Draw rectangles representing the heatmap points
+					dc.DrawRectangle(brushes[intensity], null, new Rect(x - widthHeight / 2, y - widthHeight / 2, widthHeight, widthHeight));
+				}
+
+				// Render the drawing visual onto the RenderTargetBitmap
+				rtb.Render(drawingVisual);
+			}
+
+			// Create a JpegBitmapEncoder and add the rendered bitmap to it
+			BitmapEncoder encoder = new JpegBitmapEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(rtb));
+
+			// Save the encoder's frames into a file stream as a JPG image
+			using (var stream = File.Create(filePath))
+			{
+				encoder.Save(stream);
+			}
+		}
+
+
+	}
+
+	public struct HeatPoint
+	{
+		public int X;
+		public int Y;
+		public byte Intensity;
+
+		public HeatPoint(int x, int y, byte intensity)
+		{
+			X = x;
+			Y = y;
+			Intensity = intensity;
+		}
+	}
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,84 @@
-﻿using System.Windows;
+﻿using MouseRecording.Utils;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO;
 
 namespace MouseRecording
 {
 	public partial class MainWindow : Window
 	{
+		private HeatMap heatmap;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			InitializeHeatMap();
+			DatabaseHelper.InitializeDatabase();
+		}
+
+		private void InitializeHeatMap()
+		{
+			heatmap = new HeatMap();
+			Grid.SetRow(heatmap, 1);
+			grid.Children.Add(heatmap);
+		}
+
+		private void MouseMoveHandler(object sender, MouseEventArgs e)
+		{
+			int xCoord = (int)e.GetPosition(null).X;
+			int yCoord = (int)e.GetPosition(null).Y;
+
+			DatabaseHelper.InsertMouseCoordinates(xCoord, yCoord);
+
+			// Add the mouse coordinates to the heatmap
+			heatmap.AddHeatPoint(xCoord, yCoord, 255); // You can adjust the intensity as needed
+		}
+
+		private void UpdateHeatMap()
+		{
+			heatmap.Render();
+		}
+
+		private void StartRecBtn_Click(object sender, RoutedEventArgs e)
+		{
+			MouseRecHelper.StartTracking(MouseMoveHandler);
+		}
+
+		private void Stop_Click(object sender, RoutedEventArgs e)
+		{
+			MouseRecHelper.StopTracking();
+		}
+
+		private void generateHeatmapBtn_Click(object sender, RoutedEventArgs e)
+		{
+			UpdateHeatMap();
+			ExportHeatmapAsImage(heatmap, "heatmap_image.jpg");
+			heatmap.RenderAndExport("heatmap_image.jpg");
+
+		}
+
+		private void ExportHeatmapAsImage(FrameworkElement element, string filePath)
+		{
+			// Create a RenderTargetBitmap to render the heatmap
+			RenderTargetBitmap rtb = new RenderTargetBitmap((int)element.ActualWidth, (int)element.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+			rtb.Render(element);
+
+			// Create a JPEG encoder
+			BitmapEncoder encoder = new JpegBitmapEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(rtb));
+
+			// Save the image to file
+			using (var stream = File.Create(filePath))
+			{
+				encoder.Save(stream);
+			}
+
+			MessageBox.Show($"Heatmap image saved to:\n{filePath}", "Heatmap Saved", MessageBoxButton.OK, MessageBoxImage.Information);
 		}
 	}
+
 }
