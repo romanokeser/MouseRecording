834ec9910ec2606c5c144ac8c5ab186fab793d38
rkeser <rkeser@cs.hr>
2024-05-31 18:37:51 +0200

    Render heatmap from ListView

--- ListOfRecordings.xaml.cs
+++ ListOfRecordings.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using MouseRecording.Models;
 using MouseRecording.Utils;
+using System;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace MouseRecording
 {
@@ -12,6 +16,7 @@ namespace MouseRecording
 	public partial class ListOfRecordings : Window
 	{
 		public ObservableCollection<RecordModel> Records { get; set; }
+		private string _selectedRecordingName;
 
 		public ListOfRecordings()
 		{
@@ -40,6 +45,42 @@ namespace MouseRecording
 				records.Add(record);
 			}
 			return records;
+		}
+
+		private void listViewRecordings_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			// Check if an item is selected
+			if (listViewRecordings.SelectedItem != null)
+			{
+				// Get the selected item's name
+				_selectedRecordingName = ((RecordModel)listViewRecordings.SelectedItem).Name;
+				// Show the review button
+				reviewButton.Visibility = Visibility.Visible;
+			}
+		}
+
+		private async void reviewButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Implement the review logic here
+			if (!string.IsNullOrEmpty(_selectedRecordingName))
+			{
+
+				try
+				{
+					loadingScreen.Visibility = Visibility.Visible; // Show loading screen
+																   // Initialize HeatMap and render
+					HeatMap heatMap = new HeatMap();
+					await Task.Run(() => heatMap.Render(_selectedRecordingName));
+				}
+				catch (Exception ex)
+				{
+					System.Windows.MessageBox.Show($"An error occurred: {ex.Message}");
+				}
+				finally
+				{
+					loadingScreen.Visibility = Visibility.Collapsed; // Hide loading screen
+				}
+			}
 		}
 	}
 }
